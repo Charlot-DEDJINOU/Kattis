b6284566127d7dc5ec9dce4246fc8e1c1cb25d7d
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-08-31 20:57:54 +0100

    Army Strength (Easy).py

---
+++ Army Strength (Easy).py
@@ -0,0 +1,9 @@
+for _ in range(int(input())) :
+    s = input()
+    s = input()
+    g = max(list(map(int , input().split())))
+    m = max(list(map(int , input().split())))
+    if m > g :
+        print("MechaGodzilla")
+    else :
+        print("Godzilla")
