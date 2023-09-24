8ffe661df43aa369a80735ce643a2590dee1a93b
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-09-24 22:41:04 +0100

    Almost perfect

--- Almost Perfect.py
+++ Almost Perfect.py
@@ -4,7 +4,7 @@ def parfait(n):
     somme, i = 0, 1
     sqrt_n = int(math.sqrt(n)) + 1
 
-    while i <= sqrt_n and i < n:
+    while i <= sqrt_n and i < n :
         if n % i == 0 :
             somme += i
             if i != 1 and i**2 != n :
