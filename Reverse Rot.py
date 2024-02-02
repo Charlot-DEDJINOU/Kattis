30f06d55fc3e7c5f0a204529aee4149a20e934d5
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-02 14:16:46 +0100

    Reverse Rot

---
+++ Reverse Rot.py
@@ -0,0 +1,23 @@
+alphabet_majuscules = [chr(65 + i) for i in range(26)]
+alphabet_majuscules.append("_")
+alphabet_majuscules.append(".")
+
+def ROT(message, cle) :
+    shiffer = ""
+    for letter in message :
+        shiffer += alphabet_majuscules[(alphabet_majuscules.index(letter) + cle) % 28]
+    
+    return shiffer
+
+
+while True :
+    s = input()
+    if s == "0" :
+        break
+    
+    s = s.split()
+    rotate = int(s[0])
+    message = "".join([s[1][x] for x in range(len(s[1]) - 1 , -1 ,-1)])
+
+    print(ROT(message, rotate))
+
