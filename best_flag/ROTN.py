c7b085a968c961e251553047af06a1acfefc43b4
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-30 23:45:16 +0100

    Variable ROT

---
+++ best_flag/ROTN.py
@@ -0,0 +1,44 @@
+from FileManeger import write, read
+
+def crypte(input_file, output_file, key, alphabet) : 
+    messages = read(input_file).split(chr(10))
+    content = list()
+
+    for (index,message) in enumerate(messages) :
+        content.append(crypting(message, alphabet, key + index + 1))
+
+    write(output_file, chr(10).join(content))
+
+
+def decrypte(input_file, output_file, key, alphabet) :
+    messages = read(input_file).split(chr(10))
+    content = list()
+
+    for (index,message) in enumerate(messages) :
+        content.append(decrypting(message, alphabet, key + index + 1))
+
+    write(output_file, chr(10).join(content))
+
+def crypting(text, alphabet, key):
+    encrypted_text = ''
+    for char in text:
+        if char.isalpha():
+            if char.islower():
+                encrypted_text += alphabet[(alphabet.index(char) + key) % len(alphabet)]
+            else:
+                encrypted_text += alphabet[(alphabet.index(char.lower()) + key) % len(alphabet)].upper()
+        else:
+            encrypted_text += char
+    return encrypted_text
+
+def decrypting(text, alphabet, key):
+    decrypted_text = ''
+    for char in text:
+        if char.isalpha():
+            if char.islower():
+                decrypted_text += alphabet[(alphabet.index(char) - key) % len(alphabet)]
+            else:
+                decrypted_text += alphabet[(alphabet.index(char.lower()) - key) % len(alphabet)].upper()
+        else:
+            decrypted_text += char
+    return decrypted_text
